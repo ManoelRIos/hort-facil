78857ba52b2fedca13700af3f55892fc700ee2fd
manoel neto <manoelriosneto@gmail.com>
2022-08-09 16:13:13 -0300

    Adicionando carrinho ao header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,56 @@
+/* HOME */
+
 //swipper
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets'
+    el: ".swiper-pagination",
+    type: "bullets",
   },
 
   autoplay: {
-    delay: 5000
+    delay: 5000,
   },
   // Navigation arrows
-  keyboard: true
+  keyboard: true,
 });
+
+function toggleNav() {
+  const nav = document.querySelector("#modal");
+  const btnOpen = document.querySelector(".nav-bar .icon-nav");
+  const btnClose = document.querySelector(".close-modal");
+
+  btnOpen.addEventListener("click", function () {
+    nav.classList.add("show");
+    console.log("clicked");
+  });
+
+  btnClose.addEventListener("click", function () {
+    modalCart.classList.remove("show");
+  });
+}
+
+function openCart() {
+  const modalCart = document.querySelector("#modal");
+  const btnOpen = document.querySelector(".nav-bar .icon-cart");
+  const btnClose = document.querySelector(".close-modal");
+  /* const clickOut = document.querySelector("#modal") */
+
+  btnOpen.addEventListener("click", function () {
+    modalCart.classList.add("show");    
+  });
+
+  btnClose.addEventListener("click", function () {
+    modalCart.classList.remove("show");
+  });
+
+  /* AO CLICAR FORA DO  MODAL */
+/*   clickOut.addEventListener("click", function () {
+    modalCart.classList.remove("show");
+    console.log("clicado")
+  }) */
+}
+
+toggleNav();
+openCart();
