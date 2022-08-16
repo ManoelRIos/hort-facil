143b36cae18916d9c49c074bd256b1309a94efaf
manoel neto <manoelriosneto@gmail.com>
2022-08-16 16:34:03 -0300

    Aidicionando página de meu perfil

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,31 @@
 /* HOME */
-
 function login(){
-  isLogged = true;
-  const popup = document.querySelector(".nav-bar .login-info");  
+  /* Alterar para false ou true para ver tela de usuário logado */
+  var isLogged = true;
+  const loginInfo = document.querySelector(".nav-bar .login-info");  
   const btnLogin = document.querySelector(".nav-bar .btn-login")
+  const popupUser = document.querySelector(".nav-bar .popup-user");
+  const btnUserDropdown = document.querySelector(".dropdown-user")
 
   if(isLogged){
-    popup.classList.add("logged")    
-    btnLogin.classList.add("logged")    
-  }  
+    loginInfo.classList.add("logged")    
+    btnLogin.classList.add("logged")   
 
+    const endAccount = document.querySelector("#endAccount")
+    var popupOpen = false
+
+    btnUserDropdown.addEventListener("click", () => {      
+      popupOpen = !popupOpen
+      console.log("clicado")
+      popupOpen ?  popupUser.classList.add("logged") :  popupUser.classList.remove("logged")
+    })
+
+    endAccount.addEventListener("click", () => {
+      loginInfo.classList.remove("logged")    
+      btnLogin.classList.remove("logged")   
+      isLogged = false;
+    })    
+  }  
 }
 
 function toggleNav() {
